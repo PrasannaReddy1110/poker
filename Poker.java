0fa8b5da79c616740860694ad3c154fe69410138
Nitya sri <Nitya.Nellore@cdk.com>
2016-07-09 10:36:10 +0530

    updated isStraightFlush

--- Poker.java
+++ Poker.java
@@ -2,17 +2,7 @@ import java.util.*;
 public class Poker {
     static int is3OfKind = 0;
     public  boolean isStraightFlush(int[] pips, int[] suits) {
-        for (int i : suits) {
-            if(i != suits[0]) {
-                return false;
-            }           
-        }
-        for (int i = 1; i < pips.length; i++) {
-            if (pips[i-1] + 1 != pips[i]) {
-                return false;
-            }
-        }
-        return true;
+        return ((pips[4] - pips[0] == 4) && (suits[4] == suits[0]));
     }
     public  boolean isFlush(int[] pips, int[] suits) {
         for (int i : suits) {
